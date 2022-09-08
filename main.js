ba8f5491333de7b37165ce6911a7e6d0262fb251
Ismael Gustavo Da Silva <81600908+Snoopyh@users.noreply.github.com>
2022-09-08 20:37:02 -0300

    aula 5 - projeto
    projeto quase terminado falta alguns ajustes no js

--- main.js
+++ main.js
@@ -32,21 +32,6 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
-})
-
 // SCROLL REVEAL
 ScrollReveal({
   origin: 'top',
@@ -66,3 +51,18 @@ ScrollReveal({
 #about .content,
 #about img,
 footer`)
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 1,
+      setWrapperSize: true
+    }
+  }
+})
